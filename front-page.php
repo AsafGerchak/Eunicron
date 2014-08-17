1742dc98203427a47c6c007b9189b26241d4537b
AsafGerchak <asaf.gerchak@gmail.com>
2014-08-16 20:16:55 -0400

    Making things pretty with backgrounds and font colors :princess:

--- front-page.php
+++ front-page.php
@@ -1,15 +1,15 @@
 <?php get_header();  ?>
 
-<div class="section">
-  <?php get_sidebar(); ?>
-    <div class="left">
+	<div class="section clearfix">
+	  <?php get_sidebar(); ?>
+	  <div class="left">
 			<?php // Start the loop ?>
-      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-        <!-- <h2><?php the_title(); ?></h2> -->
-        <?php the_content(); ?>
+	      <!-- <h2><?php the_title(); ?></h2> -->
+	      <?php the_content(); ?>
 
-      <?php endwhile; // end the loop?>
-    </div> <!-- /.left -->
-</div> <!-- /.section -->
+	    <?php endwhile; // end the loop?>
+	  </div> <!-- /.left -->
+	</div> <!-- /.section -->
 <?php get_footer(); ?>
