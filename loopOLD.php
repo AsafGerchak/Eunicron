2197473015cfd8f01ede5748de43a17623f17281
AsafGerchak <asaf.gerchak@gmail.com>
2014-08-18 02:42:02 -0400

    Reworking the loop for a tiled portfolio gallery :cyclone:

--- loopOLD.php
+++ loopOLD.php
@@ -13,19 +13,25 @@
 <?php endif; // end if there are no posts ?>
 
 <?php /* if there are posts, Start the Loop. */ ?>
-	
-	<h3>Portfolio</h3>
 
 <?php while ( have_posts() ) : the_post(); ?>
 
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
-			<section class="content-thumbs clearfix">
-				<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_post_thumbnail() ?></a>				
+			<section class="entry-content">
+				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?>
 				<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
 			</section><!-- .entry-content -->
+
+			<footer>
+				<p><?php the_tags('Tags: ', ', ', '<br>'); ?> Posted in <?php the_category(', '); ?> &bull; <?php edit_post_link('Edit', '', ' &bull; '); ?> <?php comments_popup_link('Respond to this post &raquo;', '1 Response &raquo;', '% Responses &raquo;'); ?></p>
+			</footer>
 				
 		</article><!-- #post-## -->
+
+		<?php comments_template( '', true ); ?>
+
 
 <?php endwhile; // End the loop. Whew. ?>
 
