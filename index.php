e462a6d96df1577da2d2a6aa0efe5c042a4f5ed9
AsafGerchak <asaf.gerchak@gmail.com>
2014-08-18 02:17:39 -0400

    Styled individual pages and links to other posts :feet:

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php get_header(); ?>
 <div class="section">
-  <div class="innerWrapper">
+    <?php get_sidebar(); ?>
     <div class="left">
     		<?php get_template_part( 'loop', 'index' );	?>
     </div> <!--/left-->
-    <?php get_sidebar(); ?>
-  </div> <!-- /.innerWrapper -->
 </div> <!-- /.section -->
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -9,17 +9,18 @@
       <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <h3 class="entry-title"><?php the_title(); ?></h3>
 
-        <div class="entry-meta">
-          <?php twentyten_posted_on(); ?>
-        </div><!-- .entry-meta -->
-
         <div class="entry-content">
           <?php the_content(); ?>
         </div><!-- .entry-content -->
 
+        <div class="entry-meta">
+           <p>Courtesy of <?php the_author_meta('first_name'); ?></p>
+           <p><?php the_date() ?></p>
+        </div><!-- .entry-meta -->
+
       </div><!-- #post-## -->
 
-      <div id="nav-below" class="navigation">
+      <div id="nav-below" class="navigation clearfix">
         <div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'twentyten' ) . '</span> %title' ); ?></div>
         <div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'twentyten' ) . '</span>' ); ?></div>
       </div><!-- #nav-below -->
